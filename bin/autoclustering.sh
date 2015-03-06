78526568b7d5da48e1161183a368c7eb4dfc614d
Jainish Shah <Jainishshah@yahoo.com>
2015-03-06 11:20:29 -0800

    Update autoclustering.sh

--- bin/autoclustering.sh
+++ bin/autoclustering.sh
@@ -1,22 +1,7 @@
-#! /bin/bash
+#!/bin/sh
 
 Host=`cat /etc/hosts | grep build_riak_1 | head -n1 | awk '{print $1}'`
 echo $Host
-echo "riak-admin cluster join \"riak@$Host\"" 
-riak-admin cluster join riak@$Host  
+echo "riak-admin cluster join \"riak@$Host\""
+riak-admin cluster join riak@$Host
 sleep 9;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
