ad967ce04d028a18f312b7a3c4d1aa278f870aeb
Jainish Shah <Jainishshah@yahoo.com>
2015-06-03 11:09:26 -0700

    Update stop-cluster.sh

--- bin/stop-cluster.sh
+++ bin/stop-cluster.sh
@@ -6,6 +6,6 @@ if env | grep -q "DOCKER_RIAK_DEBUG"; then
   set -x
 fi
 
-sudo docker ps | egrep "docker.getzephyr.com/riak" | cut -d" " -f1 | xargs -I{} sudo docker rm -f {} > /dev/null 2>&1
+sudo docker ps | egrep "docker.getzephyr.com/riak:2.1.1" | cut -d" " -f1 | xargs -I{} sudo docker rm -f {} > /dev/null 2>&1
 
 echo "Stopped the cluster and cleared all of the running containers."
